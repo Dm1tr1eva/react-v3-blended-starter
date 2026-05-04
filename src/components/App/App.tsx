33911852c032e9becb60c3e32841853f1888f396
Dm1tr1eva <114660725+Dm1tr1eva@users.noreply.github.com>
2026-05-04 11:41:45 +0200

    start project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,35 @@
 import Section from "../Section/Section";
 import Container from "../Container/Container";
+import Form from "../Form/Form";
+import { Toaster } from "react-hot-toast";
+import { useState } from "react";
+import type { Photo } from "../../types/photo";
+import { getPhotos } from "../../services/photos";
+import PhotosGallery from "../PhotosGallery/PhotosGallery";
 
 export default function App() {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  const handleSearch = async (query: string) => {
+    console.log(query);
+
+    try {
+      const fetchedPhotos = await getPhotos(query);
+      setPhotos(fetchedPhotos);
+    } catch {}
+  };
+
   return (
     <>
       <Section>
-        <Container>{/* Компоненти застосунку */}</Container>
-        Home page
+        <Container>
+          <Form onSubmit={handleSearch} />
+          {photos.length > 0 && (
+            <PhotosGallery photos={photos} onSelect={() => {}} />
+          )}
+        </Container>
       </Section>
+      <Toaster position="top-right" />
     </>
   );
 }
